6cf8912c7850e826ae84b24ebef91ff22acf4477
Himanshu kotia <himanshukotia9@gmail.com>
2025-01-21 23:33:40 +0530

    Top Selling Section Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <>
       <Navbar/>
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-8 md:px-20 py-6'>
         <Outlet />
       </main>
       <footer>Footer</footer>
